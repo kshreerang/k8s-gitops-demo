6f4800f3164b85626319a5a0d427045b446401f3
Shreerang Kaslikar <shreerang@Shreerangs-MacBook-Air.local>
2026-01-17 00:16:57 +0530

    update app message to test full pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Shreerang! I am running inside Kubernetes on your MacBook!"
+    return "Hello DevOps Engineer! The CI/CD Pipeline is fully operational!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
